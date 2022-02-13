9d3d0bbc4718a9a0c82c78f2ede217222c176099
apple <apple@MacBook-Pro-cua-apple.local>
2022-02-13 10:38:25 +0700

    fix db

--- app/controllers/bests_controller.rb
+++ app/controllers/bests_controller.rb
@@ -4,6 +4,5 @@ class BestsController < ApplicationController
   def index
     @doc = Nokogiri::HTML(URI.open("https://news.ycombinator.com/best"))
     puts Readability::Document.new(@doc.serialize).content
-    binding.pry
   end
 end
